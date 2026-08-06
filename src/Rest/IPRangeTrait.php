21fa56ca4d6358fbedb678d9ae8151f4f8285fa9
Hamish <hamish.slater@hallowelt.com>
2026-08-06 13:51:22 +0700

    Update Config namespace
    ERM48053

--- src/Rest/IPRangeTrait.php
+++ src/Rest/IPRangeTrait.php
@@ -2,7 +2,7 @@
 
 namespace MediaWiki\Extension\WikiRAG\Rest;
 
-use Config;
+use MediaWiki\Config\Config;
 use MediaWiki\Context\RequestContext;
 use MediaWiki\Rest\HttpException;
 use MWException;
